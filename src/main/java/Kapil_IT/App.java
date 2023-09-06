08f89763013496473709244894be4014a991e9ce
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2023-09-06 12:59:57 +0530

    Update App.java

--- src/main/java/Kapil_IT/App.java
+++ src/main/java/Kapil_IT/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Webhook Job Demo");
+        System.out.println("KIT Webhook Job Configuration Demo");
     }
     
 }
